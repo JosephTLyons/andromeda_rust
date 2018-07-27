cc24f1710508c2e8444293fc77d29903edde19fa
Joseph Lyons <josephtlyons@gmail.com>
2018-07-27 14:22:55 -0400

    Updating work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+extern crate rand;
+
+use rand::prelude::*;
+
 fn generate_serial_numbers (number_of_serials: u32, length_of_serial: u32) {
     let mut alphabet_vector: Vec<u8> = create_alphabet_vector (false, false, true);
 
@@ -5,27 +9,33 @@ fn generate_serial_numbers (number_of_serials: u32, length_of_serial: u32) {
     //     serial_vector.push (create_alphabet_vector());
     // }
 
-    for x in &alphabet_vector {
-        println! ("{}", *x as char);
+    let mut rng = thread_rng();
+    let mut random_num;
+
+    while alphabet_vector.len() > 0 {
+        if rng.gen() { // random bool
+            random_num = rng.gen_range (0, alphabet_vector.len());
+            println! ("{}", alphabet_vector.remove(random_num) as char);
+        }
     }
 }
 
 fn create_alphabet_vector (number: bool, uppercase: bool, lowercase: bool) -> Vec<u8> {
     let mut v: Vec<u8> = Vec::new();
 
-    if (number) {
+    if number {
         for x in '0' as u8..'9' as u8 + 1 {
             v.push (x);
         }
     }
 
-    if (uppercase) {
+    if uppercase {
         for x in 'A' as u8..'Z' as u8 + 1 {
             v.push (x);
         }
     }
 
-    if (lowercase) {
+    if lowercase {
         for x in 'a' as u8..'z' as u8 + 1 {
             v.push (x);
         }
