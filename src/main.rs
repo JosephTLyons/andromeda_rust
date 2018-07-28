5817c845dcfb200aa238475f16de7be047770bf8
Joseph Lyons <josephtlyons@gmail.com>
2018-07-28 03:06:32 -0400

    Moving towards a new implementation idea

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,24 @@ use rand::prelude::*;
 
 fn generate_serial_numbers (number_of_serials: u32, length_of_serial: u32,
                             number: bool, uppercase: bool, lowercase: bool) {
-    let mut alphabet_vector: Vec<u8> = create_alphabet_vector (number, uppercase, lowercase);
+    let mut values_vector: Vec<u8> = create_values_vector (number, uppercase, lowercase);
 
     // for x in 0..number_of_serials {
-    //     serial_vector.push (create_alphabet_vector());
+    //     serial_vector.push (create_values_vector());
     // }
 
     let mut rng = thread_rng();
-    let mut random_num;
+    thread_rng().shuffle (&mut values_vector);
+    print_vector (values_vector);
+}
 
-    while alphabet_vector.len() > 0 {
-        if rng.gen() { // random bool
-            random_num = rng.gen_range (0, alphabet_vector.len());
-            println! ("{}", alphabet_vector.remove(random_num) as char);
-        }
+fn print_vector (vec: Vec<u8>) {
+    for x in 0..vec.len() {
+        println! ("{}", vec[x] as char);
     }
 }
 
-fn create_alphabet_vector (number: bool, uppercase: bool, lowercase: bool) -> Vec<u8> {
+fn create_values_vector (number: bool, uppercase: bool, lowercase: bool) -> Vec<u8> {
     let mut v: Vec<u8> = Vec::new();
 
     if number {
@@ -46,5 +46,5 @@ fn create_alphabet_vector (number: bool, uppercase: bool, lowercase: bool) -> Ve
 }
 
 fn main() {
-    generate_serial_numbers (100, 8, true, true, true);
+    generate_serial_numbers (100, 8, true, false, false);
 }
