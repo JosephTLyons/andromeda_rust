f3fd6485f7eb4fed22d7a080526070806ee747df
Joseph Lyons <josephtlyons@gmail.com>
2018-07-29 01:43:00 -0400

    Renamed vector_of_values_vector to vector_of_character_vectors

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,14 @@ use rand::prelude::*;
 fn generate_serial_numbers (number_of_serials: u32, length_of_serial: u32,
                             number: bool, uppercase: bool, lowercase: bool) {
     let mut character_vector: Vec<u8> = create_values_vector (number, uppercase, lowercase);
-    let mut vector_of_values_vector: Vec<Vec<u8>> = Vec::new();
+    let mut vector_of_character_vectors: Vec<Vec<u8>> = Vec::new();
 
     for x in 0..length_of_serial {
         thread_rng().shuffle (&mut character_vector);
-        vector_of_values_vector.push (character_vector.clone());
+        vector_of_character_vectors.push (character_vector.clone());
     }
 
-    print_serial_numbers_to_file (vector_of_values_vector);
+    print_serial_numbers_to_file (vector_of_character_vectors);
 }
 
 // Delete this when done
@@ -46,26 +46,26 @@ fn create_values_vector (number: bool, uppercase: bool, lowercase: bool) -> Vec<
     return v;
 }
 
-fn print_serial_numbers_to_file (vector_of_values_vector: Vec<Vec<u8>>) {
+fn print_serial_numbers_to_file (vector_of_character_vectors: Vec<Vec<u8>>) {
 
-    let total_possible_combinations: i64 = i64::pow (vector_of_values_vector[0].len() as i64,
-                                                     vector_of_values_vector.len() as u32);
+    let total_possible_combinations: i64 = i64::pow (vector_of_character_vectors[0].len() as i64,
+                                                     vector_of_character_vectors.len() as u32);
 
     println! ("{} total combinations possible with serial numbers of length {} with {} characters choices",
               total_possible_combinations,
-              vector_of_values_vector.len(),
-              vector_of_values_vector[0].len());
+              vector_of_character_vectors.len(),
+              vector_of_character_vectors[0].len());
 
-    //let mut index_vector = vec![0; vector_of_values_vector.len()];
+    //let mut index_vector = vec![0; vector_of_character_vectors.len()];
 
     //print_vector (index_vector);
 
-    // for x in 0..vector_of_values_vector.len() {
-    //     for x in 0..vector_of_values_vector[x] {
+    // for x in 0..vector_of_character_vectors.len() {
+    //     for x in 0..vector_of_character_vectors[x] {
     //
     //     }
     // }
-    // Use length of vector_of_values_vector in for loop to move across vectors and then an inner loop for each vector
+    // Use length of vector_of_character_vectors in for loop to move across vectors and then an inner loop for each vector
 }
 
 fn main() {
